321024fbc30c6fdc407c859dd3d8e5f4b6568c80
tassyosilva <tassyoms22@gmail.com>
2025-10-22 15:02:11 -0400

    Reformatação do arquivo eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
-    root: true,
+  root: true,
 
-    extends: [
-        '@react-native',
-        'prettier',
-    ],
+  extends: ["@react-native", "prettier"],
 
-    rules: {
-        'react-native/no-inline-styles': 'warn',
-        'react-hooks/exhaustive-deps': 'error',
-    },
+  rules: {
+    "react-native/no-inline-styles": "warn",
+    "react-hooks/exhaustive-deps": "error",
+  },
 };
